61e1b0362a4ee1bae67fa2fff710e4c75b6b418a
MatheusEli <matheuselisouzaferreira@hotmail.com>
2021-01-16 18:37:26 -0300

    usando diretiva *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  photos = [{
+    url: 'https://rd1.com.br/wp-content/uploads/2020/11/20201110-mandalorian.jpg',
+    description: 'The Mandalorian'
+  },{
+    url: 'https://s2.glbimg.com/AN4Gw4fFNwjVdzG_oRCYN_-cvXI=/512x320/smart/e.glbimg.com/og/ed/f/original/2020/11/30/baby-yoda.jpg',
+    description: 'Bebe Yoda - The Mandalorian'
+  }]
+}
